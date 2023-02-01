24f9366701cdcacd9c26cd5d20ff75e3718dc5d8
Du, Jun <jun.du@intel.com>
2023-02-01 02:59:25 +0000

    update launch_benchmark.sh

---
+++ launch_benchmark.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+set -xe
+
+function main {
+    source oob-common/common.sh
+    # set common info
+    init_params $@
+    fetch_device_info
+    set_environment
+
+    # requirements
+    pip uninstall slowfast -y
+    # detectron2
+    rm -rf detectron2_repo && git clone https://github.com/facebookresearch/detectron2 detectron2_repo
+    pip install -e detectron2_repo
+    # pytorchvideo
+    rm -rf pytorchvideo && git clone https://github.com/facebookresearch/pytorchvideo.git
+    cd pytorchvideo && git checkout 06360f462d30765943cf8ab6c228ca88c86aabbf
+    python setup.py install
+    # slowfast
+    cd ..
+    pip install -e .
+
+    # if multiple use 'xxx,xxx,xxx'
+    model_name_list=($(echo "${model_name}" |sed 's/,/ /g'))
+    batch_size_list=($(echo "${batch_size}" |sed 's/,/ /g'))
+
+    # generate benchmark
+    for model_name in ${model_name_list[@]}
+    do
+        #
+        for batch_size in ${batch_size_list[@]}
+        do
+            # clean workspace
+            logs_path_clean
+
+            # generate launch script for multiple instance
+            if [ "${OOB_USE_LAUNCHER}" == "1" ] && [ "${device}" == "cpu" ];then
+                generate_core_launcher
+            else
+                generate_core
+            fi
+            # launch
+            echo -e "\n\n\n\n Running..."
+            source ${excute_cmd_file}
+            echo -e "Finished.\n\n\n\n"
+            # collect launch result
+            collect_perf_logs
+        done
+    done
+}
+
+# run
+function generate_core {
+    # generate multiple instance script
+    for(( i=0; i<instance; i++ ))
+    do
+        real_cores_per_instance=$(echo ${device_array[i]} |awk -F, '{print NF}')
+        log_file="${log_dir}/rcpi${real_cores_per_instance}-ins${i}.log"
+
+        # instances
+        if [ "${device}" == "cpu" ];then
+            OOB_EXEC_HEADER=" numactl -m $(echo ${device_array[i]} |awk -F ';' '{print $2}') "
+            OOB_EXEC_HEADER+=" -C $(echo ${device_array[i]} |awk -F ';' '{print $1}') "
+        elif [ "${device}" == "cuda" ];then
+            OOB_EXEC_HEADER=" CUDA_VISIBLE_DEVICES=${device_array[i]} "
+	    if [[ "${mode_name}" == "realtime" ]];then
+	        addtion_options+=" --nv_fuser "
+	    fi
+	fi
+        printf " ${OOB_EXEC_HEADER} \
+	    python tools/run_net.py --cfg ${CONFIG_FILE} \
+                --num_warmup ${num_warmup} --num_iter ${num_iter} \
+                --batch_size ${batch_size} \
+                --precision ${precision} \
+                --channels_last ${channels_last} \
+		--device ${device} \
+		--jit \
+                ${addtion_options} \
+        > ${log_file} 2>&1 &  \n" |tee -a ${excute_cmd_file}
+        if [ "${numa_nodes_use}" == "0" ];then
+            break
+        fi
+    done
+    echo -e "\n wait" >> ${excute_cmd_file}
+}
+
+function generate_core_launcher {
+    # generate multiple instance script
+    for(( i=0; i<instance; i++ ))
+    do
+        real_cores_per_instance=$(echo ${device_array[i]} |awk -F, '{print NF}')
+        log_file="${log_dir}/rcpi${real_cores_per_instance}-ins${i}.log"
+
+        printf "python -m launch --enable_jemalloc \
+                    --core_list $(echo ${device_array[@]} |sed 's/;.//g') \
+                    --log_file_prefix rcpi${real_cores_per_instance} \
+                    --log_path ${log_dir} \
+                    --ninstances ${#device_array[@]} \
+                    --ncore_per_instance ${real_cores_per_instance} \
+            tools/infer.py --weights $CKPT_DIR \
+                --source $DATASET_DIR \
+                --num_iter $num_iter --num_warmup $num_warmup \
+                --channels_last $channels_last --precision $precision \
+                ${addtion_options} \
+        > /dev/null 2>&1 &  \n" |tee -a ${excute_cmd_file}
+        break
+    done
+    echo -e "\n wait" >> ${excute_cmd_file}
+    # download launcher
+    wget --no-proxy -O launch.py http://mengfeil-ubuntu.sh.intel.com/share/launch.py
+}
+
+# download common files
+rm -rf oob-common && git clone https://github.com/intel-sandbox/oob-common.git -b gpu_oob
+
+# Start
+main "$@"
